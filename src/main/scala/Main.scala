c069e3a3d703aa2afbeb5368c00aed79966ae78f
Miles Higbie <miles.higbie@gmail.com>
2022-02-12 13:35:58 -0500

    updated inputs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,17 @@
 import scala.io.StdIn.readLine
 
   object Main extends App{
-    print("Enter your first name: ")
+    println("Hello and welcome to Scala Mad libs!")
+    println("Can I please have your first name?")
     val firstName = readLine()
+    println("Can I please have your last name?")
+    val lastName = readLine()
+    println(s"Great $firstName $lastName, lets begin!")
+    println("What is your age?")
+    val age = scala.io.StdIn.readInt()
+    val mortalityRateMinusAge = 78 - age
+    val timeLeftinMonths = mortalityRateMinusAge * 12
+    val timeLeftinMinutes = 525600 * mortalityRateMinusAge
+    println(s"The avg Mortality rate in the US is 78. You have $mortalityRateMinusAge years, $timeLeftinMonths months, and $timeLeftinMinutes minutes left to live")
+    println("Good luck!")
   }
